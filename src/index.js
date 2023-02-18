63894a27e363adff59a86e4609347dea373a84cc
Will <rut.willems@gmail.com>
2023-02-18 13:46:22 +0100

    add attempt function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-export const myPackage = (taco = '') => `${taco} from my package`;
+/**
+ * Wraps a function in a try/catch block and returns an array with the error and result
+ * @param {function} func - Function to be executed
+ * @returns {function(...args: any[]): Promise<[err: Error | null, result: any]>}}}
+ */
+export const attempt = function attempt(func) {
+   return async function (...args) {
+      try {
+         return [null, await func(...args)]
+      } catch (err) {
+         return [err, undefined]
+      }
+   }
+}
